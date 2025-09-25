5d3a0f567560cd49db2cf3c1330f18b1b458a449
Mohammad-Amirian <m.amirianmatlob@dal.ca>
2025-09-25 11:36:51 -0300

    version update is added.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
-        "piCurve 0.2.4 loaded:: Windows parallel bug in Fit_piModel() fixed."
+        # "piCurve 0.2.4 loaded:: Windows parallel bug in Fit_piModel() fixed."
+        "piCurve 0.2.5 loaded:: DataType_piCurve handles imbalanced more delicately now using both adjR2 & AIC"
     )
 }
