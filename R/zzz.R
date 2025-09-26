a3d0c9344a00166c4b8d0feef089e2d1364475c7
Mohammad-Amirian <m.amirianmatlob@dal.ca>
2025-09-26 11:00:56 -0300

    new update released

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
         # "piCurve 0.2.4 loaded:: Windows parallel bug in Fit_piModel() fixed."
-        "piCurve 0.2.5 loaded:: DataType_piCurve handles imbalanced more delicately now using both adjR2 & AIC"
+        # "piCurve 0.2.5 loaded:: DataType_piCurve handles imbalanced more delicately now using both adjR2 & AIC"
+        "piCurve 0.2.6 loaded:: DataType_piCurve updated, handling imbalanced photoinhibition data more delicately with dection limit of 5% drop in PR"
     )
 }
