b1b4141cca0dd7bdcc2a15eb5a6d2fb71f3e5857
Mohammad-Amirian <m.amirianmatlob@dal.ca>
2025-09-26 14:17:03 -0300

    version update

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@
     packageStartupMessage(
         # "piCurve 0.2.4 loaded:: Windows parallel bug in Fit_piModel() fixed."
         # "piCurve 0.2.5 loaded:: DataType_piCurve handles imbalanced more delicately now using both adjR2 & AIC"
-        "piCurve 0.2.6 loaded:: DataType_piCurve updated, handling imbalanced photoinhibition data more delicately with dection limit of 5% drop in PR"
+        # "piCurve 0.2.6 loaded:: DataType_piCurve updated, handling imbalanced photoinhibition data more delicately with detection limit of 5% drop in PR"
+        "piCurve 0.2.7 loaded:: DataType_piCurve debuged, handling imbalanced data more delicately."
     )
 }
