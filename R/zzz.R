0738af21b51f67476e84369609b1f0550b0996d5
Mohammad-Amirian <m.amirianmatlob@dal.ca>
2025-10-23 17:34:11 -0300

    updating to version 0.3.1

--- R/zzz.R
+++ R/zzz.R
@@ -3,9 +3,11 @@
         # "piCurve 0.2.4 loaded:: Windows parallel bug in Fit_piModel() fixed."
         # "piCurve 0.2.5 loaded:: DataType_piCurve handles imbalanced more delicately now using both adjR2 & AIC"
         # "piCurve 0.2.6 loaded:: DataType_piCurve updated, handling imbalanced photoinhibition data more delicately with detection limit of 5% drop in PR"
-        # "piCurve 0.2.7 loaded:: DataType_piCurve debuged, handling imbalanced data more delicately."
+        # "piCurve 0.2.7 loaded:: DataType_piCurve debugged, handling imbalanced data more delicately."
         # "piCurve 0.2.8 loaded:: bugs fixed; default parallel computing choice is set on 1 for Windows users."
         # "piCurve 0.2.9 loaded:: Improved data classification: added R2 threshold (0.5) for high-variance cases."
-        "piCurve 0.3.0 loaded:: DarkReactionRate_R option is added to the optimizer; see ?Fit_piModel"
+        # "piCurve 0.3.0 loaded:: DarkReactionRate_R option is added to the optimizer; see ?Fit_piModel"
+        "piCurve 0.3.1 loaded::  Plot_piCurve debugged"
     )
 }
+
