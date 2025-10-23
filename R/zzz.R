2cc27690d72576c288d2f515759b1bb146b13942
Mohammad-Amirian <m.amirianmatlob@dal.ca>
2025-10-23 09:53:10 -0300

    version 0.2.9 loaded

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,7 @@
         # "piCurve 0.2.5 loaded:: DataType_piCurve handles imbalanced more delicately now using both adjR2 & AIC"
         # "piCurve 0.2.6 loaded:: DataType_piCurve updated, handling imbalanced photoinhibition data more delicately with detection limit of 5% drop in PR"
         # "piCurve 0.2.7 loaded:: DataType_piCurve debuged, handling imbalanced data more delicately."
-        "piCurve 0.2.8 loaded:: bugs fixed; default parallel computing choice is set on 1 for Windows users."
+        # "piCurve 0.2.8 loaded:: bugs fixed; default parallel computing choice is set on 1 for Windows users."
+        "piCurve 0.2.9 loaded:: Improved data classification: added R2 threshold (0.5) for high-variance cases."
     )
 }
